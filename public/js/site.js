dfb199d1861755b4e5f57c726aee58ba8374c136
garvin <huckels@uscreen.de>
2021-03-01 09:16:19 +0100

    add js form burger menu

--- public/js/site.js
+++ public/js/site.js
@@ -7,7 +7,29 @@
   \******************************/
 /***/ (() => {
 
-// This is all you.
+function menuOpener() {
+  var menu = document.querySelector(".menu-mobile");
+  var body = document.querySelector(".body");
+  var overlayer = document.querySelector(".menu-overlayer");
+  document.querySelector(".menu-opener").addEventListener("click", function (e) {
+    body.classList.toggle('overflow-hidden');
+    menu.classList.toggle('hidden');
+    overlayer.classList.toggle('hidden');
+  });
+  document.querySelector(".menu-closer").addEventListener("click", function (e) {
+    body.classList.toggle('overflow-hidden');
+    menu.classList.toggle('hidden');
+    overlayer.classList.toggle('hidden');
+  });
+}
+
+var ready = function ready(callback) {
+  if (document.readyState != "loading") callback();else document.addEventListener("DOMContentLoaded", callback);
+};
+
+ready(function () {
+  menuOpener();
+});
 
 /***/ }),
 
--- resources/js/site.js
+++ resources/js/site.js
@@ -1,1 +1,26 @@
-// This is all you.
+function menuOpener() {
+    var menu = document.querySelector(".menu-mobile");
+    var body = document.querySelector(".body");
+    var overlayer = document.querySelector(".menu-overlayer");
+
+    document.querySelector(".menu-opener").addEventListener("click", (e) => {
+        body.classList.toggle('overflow-hidden')
+        menu.classList.toggle('hidden')
+        overlayer.classList.toggle('hidden')
+    });
+
+    document.querySelector(".menu-closer").addEventListener("click", (e) => {
+        body.classList.toggle('overflow-hidden')
+        menu.classList.toggle('hidden')
+        overlayer.classList.toggle('hidden')
+    });
+}
+
+var ready = (callback) => {
+  if (document.readyState != "loading") callback();
+  else document.addEventListener("DOMContentLoaded", callback);
+}
+
+ready(() => {
+    menuOpener();
+});
