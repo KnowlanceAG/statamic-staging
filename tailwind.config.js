d61d01915a6e6191ed4089cb940c39cc602ff364
garvin <huckels@uscreen.de>
2021-03-10 17:15:02 +0100

    add grid classes to purge safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
         './resources/**/*.antlers.html',
         './resources/**/*.blade.php',
         './content/**/*.md'
-      ]
+        ],
+        options: {
+            safelist: [
+                'lg:grid-cols-1',
+                'lg:grid-cols-2',
+                'lg:grid-cols-3',
+                'lg:grid-cols-4',
+                'lg:grid-cols-5',
+                'lg:grid-cols-6',
+                'lg:grid-cols-7',
+                'lg:grid-cols-8',
+                'lg:grid-cols-9',
+                'lg:grid-cols-10',
+                'lg:grid-cols-11',
+                'lg:grid-cols-12',
+            ],
+        }
     },
     important: true,
     theme: {
