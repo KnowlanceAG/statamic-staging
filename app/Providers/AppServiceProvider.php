b6aaaab635c7d395fe22f1dead8db480994b70fe
Charan Sarin <sarin@uscreen.net>
2021-03-29 17:48:29 +0200

    Added hook for state reset after ssg process. #12043

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,11 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Statamic\Statamic;
+use Statamic\StaticSite\Generator;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Log;
+
+use Uscreen\Cpssg\Http\Controllers\CpssgController;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,9 +27,21 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Generator $ssg = null)
     {
         // Statamic::script('app', 'cp');
         Statamic::style('app', 'cp.css');
+        if ($ssg) {
+            $ssg->after(function () {
+                // restore symlink
+                Log::debug('Restoring symlink.');
+                $staticDir = base_path() . '/storage/app/static';
+                $outLink =  base_path() . '/storage/app/out';
+                File::delete($outLink);
+                File::link($staticDir, $outLink);
+                File::delete(CpssgController::$lockfileName);
+                Log::debug('SSG finished.');
+            });
+        }
     }
 }
