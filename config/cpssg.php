e8d3880c035070194eefab33d7fea992e2c02f12
Charan Sarin <sarin@uscreen.net>
2021-03-31 15:01:26 +0200

    Added published addon files (config, language)

---
+++ config/cpssg.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+  /*
+  |--------------------------------------------------------------------------
+  | CORS Allowed Hosts
+  |--------------------------------------------------------------------------
+  |
+  | This option controls which hosts will be allowed to retrieve a fresh token
+  | from the dynamic backend.
+  | Expecting a comma-separated list of hosts matching the referrer of the
+  | requestion resource.
+  */
+  'allowed_hosts' => explode(',', preg_replace('/\s+/', '', env('CPSSG_ALLOWED_HOSTS', '')))
+
+];
