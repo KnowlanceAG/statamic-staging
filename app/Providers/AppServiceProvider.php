95bcb2bfd2bb0ba22d8364e0ebff3fafa1abba09
Charan Sarin <sarin@uscreen.net>
2021-04-19 18:15:42 +0200

    Test url pagination rewrite.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,13 @@
 
 namespace App\Providers;
 
+use App\UrlPaginator as UrlPaginator;
+use Statamic\Extensions\Pagination\LengthAwarePaginator;
+
 use Illuminate\Support\ServiceProvider;
 use Statamic\Statamic;
 use Statamic\StaticSite\Generator;
+use Statamic\Facades\Entry;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
 
@@ -29,6 +33,36 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(Generator $ssg = null)
     {
+        $this->app->extend(LengthAwarePaginator::class, function ($paginator) {
+            $options = $paginator->getOptions();
+            $options['path'] = preg_replace('/\/page\/\d+$/', '', $options['path']);
+
+            return $this->app->makeWith(UrlPaginator::class, [
+                'items' => $paginator->getCollection(),
+                'total' => $paginator->total(),
+                'perPage' => $paginator->perPage(),
+                'currentPage' => $paginator->currentPage(),
+                'options' => $options,
+            ]);
+        });
+
+        UrlPaginator::currentPageResolver(function () {
+            return optional($this->app['request']->route())->parameter('page');
+        });
+
+        $this->app->beforeResolving(Generator::class, function ($generator) {
+            $config = config('statamic.ssg');
+
+            $config['urls'] = array_merge(
+                $config['urls'],
+                $this->blogUrls()
+                // $this->articleUrls(),
+                // $this->tagUrls(),
+            );
+
+            config(['statamic.ssg' => $config]);
+        });
+
         // Statamic::script('app', 'cp');
         Statamic::style('app', 'cp.css');
         if ($ssg) {
@@ -54,5 +88,22 @@ class AppServiceProvider extends ServiceProvider
                 Log::debug('SSG finished!');
             });
         }
+    }
+
+    private function blogUrls()
+    {
+        // The URL of the listing.
+        $url = '/blog';
+
+        // The number of entries per page, according to your collection tag.
+        $perPage = 10;
+
+        // The total number of entries in the collection.
+        // Make sure to mimic whatever params/filters are on the collection tag.
+        $total = Entry::query()->where('collection', 'blog')->where('status', 'published')->count();
+
+        return collect(range(1, ceil($total / $perPage)))
+            ->map(fn ($page) => $url.'/page/'.$page)
+            ->all();
     }
 }
