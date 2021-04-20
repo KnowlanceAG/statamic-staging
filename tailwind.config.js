c29961cd5cd2454e58aa24247dfc1718a172a306
garvin <huckels@uscreen.de>
2021-04-20 16:34:09 +0200

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
         ],
         options: {
             safelist: [
+                'grid-cols-1',
+                'grid-cols-2',
+                'grid-cols-3',
+                'grid-cols-4',
+                'grid-cols-5',
+                'grid-cols-6',
                 'lg:grid-cols-1',
                 'lg:grid-cols-2',
                 'lg:grid-cols-3',
@@ -25,6 +31,7 @@ module.exports = {
                 'md:float-right',
                 'md:float-left',
                 'md:mx-auto',
+                'content-center',
             ],
         }
     },
