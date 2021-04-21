d1fa042b0c0628c0e8c83695fdddcd955575db1c
Charan Sarin <sarin@uscreen.net>
2021-04-21 12:49:28 +0200

    Handle pagination related ssg logic in AppServiceProvider.php.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,8 +6,32 @@ use Illuminate\Support\ServiceProvider;
 use Statamic\Statamic;
 use Statamic\StaticSite\Generator;
 
+use Statamic\Facades\Entry;
+use App\UrlPaginator as UrlPaginator;
+use Statamic\Extensions\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Log;
+
 class AppServiceProvider extends ServiceProvider
 {
+    private $ssgConfig;
+
+    private function blogUrls()
+    {
+      // The URL of the listing.
+      $url = '/blog';
+
+      // The number of entries per page, according to your collection tag.
+      $perPage = 10;
+
+      // The total number of entries in the collection.
+      // Make sure to mimic whatever params/filters are on the collection tag.
+      $total = Entry::query()->where('collection', 'blog')->where('status', 'published')->count();
+
+      return collect(range(1, ceil($total / $perPage)))
+        ->map(fn ($page) => $url.'/page/'.$page)
+        ->all();
+    }
+
     /**
      * Register any application services.
      *
@@ -25,7 +49,49 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(Generator $ssg = null)
     {
-        // Statamic::script('app', 'cp');
-        Statamic::style('app', 'cp.css');
+      // Statamic::script('app', 'cp');
+      Statamic::style('app', 'cp.css');
+
+      $this->app->extend(LengthAwarePaginator::class, function ($paginator) {
+        $options = $paginator->getOptions();
+        $options['path'] = preg_replace('/\/page\/\d+$/', '', $options['path']);
+
+        return $this->app->makeWith(UrlPaginator::class, [
+            'items' => $paginator->getCollection(),
+            'total' => $paginator->total(),
+            'perPage' => $paginator->perPage(),
+            'currentPage' => $paginator->currentPage(),
+            'options' => $options,
+        ]);
+      });
+
+      UrlPaginator::currentPageResolver(function () {
+          return optional($this->app['request']->route())->parameter('page');
+      });
+
+
+      if (!isset($this->ssgConfig)) {
+        $this->ssgConfig = config('statamic.ssg');
+        $this->ssgConfig['base_url'] = config('cpssg.static_site_url');
+        $this->ssgConfig['urls'] = array_merge(
+            $this->ssgConfig['urls'],
+            $this->blogUrls()
+        );
+
+        config(['statamic.ssg' => $this->ssgConfig]);
+        if ($ssg) {
+          $ssg->setConfig(config('statamic.ssg'));
+        }
+      }
+
+      $this->app->beforeResolving(Generator::class, function ($generator) {
+        $config = config('statamic.ssg');
+        $config['base_url'] = config('cpssg.static_site_url');
+        $config['urls'] = array_merge(
+            $config['urls'],
+            $this->blogUrls()
+        );
+        config(['statamic.ssg' => $this->ssgConfig]);
+      });
     }
 }
