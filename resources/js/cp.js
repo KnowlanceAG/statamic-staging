b4779d21b6985475c16535ffbb1af70f4f025ea2
Charan Sarin <sarin@uscreen.net>
2021-08-10 11:00:13 +0200

    Embed custom cp script for #12869.

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -3,12 +3,26 @@
  * https://statamic.dev/extending/control-panel
  */
 
-/** Example Fieldtype
-
-import ExampleFieldtype from './components/fieldtypes/ExampleFieldtype.vue';
+const contextCfg = {
+  fachrichtungen: 'e45d6722-981e-4f80-bc4c-3c767caf4b6d',
+  arbeiten: 'd118c7b2-141d-4bac-972d-c1502381a340'
+}
 
 Statamic.booting(() => {
-    Statamic.$components.register('example-fieldtype', ExampleFieldtype);
-});
+  Statamic.$conditions.add('parentIsFachrichtungen', ({ values }) => {
+    return (values.parent || [])[0] === contextCfg.fachrichtungen
+  })
 
-*/
+  Statamic.$conditions.add('parentIsArbeiten', ({ values }) => {
+    return (values.parent || [])[0] === contextCfg.arbeiten
+  })
+
+  Statamic.$conditions.add('noContextConfig', ({ values }) => {
+    const parent = (values.parent || [])[0]
+    if (!parent) return true
+
+    return !Object.keys(contextCfg).map(
+      (k) => contextCfg[k]
+    ).includes(parent)
+  })
+})
