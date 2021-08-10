1fe7adb76ff784ef8996c2defc5c37e3f3a535e5
Charan Sarin <sarin@uscreen.net>
2021-08-10 11:08:22 +0200

    Simplified context for #12869.

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -3,26 +3,12 @@
  * https://statamic.dev/extending/control-panel
  */
 
-const contextCfg = {
-  fachrichtungen: 'e45d6722-981e-4f80-bc4c-3c767caf4b6d',
-  arbeiten: 'd118c7b2-141d-4bac-972d-c1502381a340'
-}
-
 Statamic.booting(() => {
   Statamic.$conditions.add('parentIsFachrichtungen', ({ values }) => {
-    return (values.parent || [])[0] === contextCfg.fachrichtungen
+    return (values.parent || [])[0] === 'e45d6722-981e-4f80-bc4c-3c767caf4b6d'
   })
 
   Statamic.$conditions.add('parentIsArbeiten', ({ values }) => {
-    return (values.parent || [])[0] === contextCfg.arbeiten
-  })
-
-  Statamic.$conditions.add('noContextConfig', ({ values }) => {
-    const parent = (values.parent || [])[0]
-    if (!parent) return true
-
-    return !Object.keys(contextCfg).map(
-      (k) => contextCfg[k]
-    ).includes(parent)
+    return (values.parent || [])[0] === 'd118c7b2-141d-4bac-972d-c1502381a340'
   })
 })
