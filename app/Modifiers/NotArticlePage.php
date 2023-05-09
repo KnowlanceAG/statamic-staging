12593646f21540bfbfe43fca500c0d2396c315a8
Charan Sarin <sarin@uscreen.de>
2023-05-09 13:35:30 +0200

    #14228 Moved schema script; added modifier to detect non-article pages

---
+++ app/Modifiers/NotArticlePage.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Modifiers;
+
+use Statamic\Modifiers\Modifier;
+// use Illuminate\Support\Facades\Log;
+
+class NotArticlePage extends Modifier
+{
+
+    protected static $handle = 'not_article_page';
+
+    private $pattern = '/\/(presse|blog)\/[\-a-zA-Z0-9]+[\/]?$/';
+
+    /**
+     * Check if url is not a pagination page.
+     *
+     * @param mixed  $value    The url
+     * @param array  $params   Any parameters used in the modifier
+     * @param array  $context  Contextual values
+     * @return boolean
+     */
+    public function index($value, $params, $context)
+    {
+        return preg_match($this->pattern, $value, $matches) == 0;
+    }
+}
