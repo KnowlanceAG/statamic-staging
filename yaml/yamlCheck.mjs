9dd8079e7f9aac905bf1c96beb6bfb292f5b1455
Charan Sarin <sarin@uscreen.de>
2024-01-23 12:11:47 +0100

    Adjusted yaml check to identify irregular marks object.

--- yaml/yamlCheck.mjs
+++ yaml/yamlCheck.mjs
@@ -9,16 +9,24 @@ import traverse from 'traverse'
 
 console.time('all')
 
-const filesStream = globStream(['yaml-test/content/collections/**/*.md'])
+const filesStream = globStream(['./content/collections/**/*.md'])
+
+const marksObject = {}
 
 for await (const file of filesStream) {
+  console.log('checking', file)
   const yamlDocs = yaml.loadAll(await readFile(file, 'utf8'))
   const doc = yamlDocs[0]
 
   traverse(doc).forEach(function (x) {
-    if (x && x.marks && x.marks.find((e) => e.type === 'textAlign')) {
+    if (x?.marks?.length && x.marks.find((e) => e.type === 'textAlign')) {
       console.log('we are doomed', file)
+    } else if (x && x.marks && x.marks.length === undefined) {
+      marksObject[file] = marksObject[file] ? marksObject[file] + 1 : 1
     }
   })
 }
 console.timeEnd('all')
+
+console.log('marksObject', marksObject)
+console.log('marksObject.length', Object.keys(marksObject).length)
